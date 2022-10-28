08095e984eb8e018a15bb24cb1e9fe3710e0d4c7
Lautaro Barba <lau_mb94@hotmail.com>
2022-10-28 01:54:52 -0300

    Placeholder para el componente tabla

---
+++ app/src/components/MaterialTable.tsx
@@ -0,0 +1,11 @@
+import Table from "@mui/material/Table";
+
+// interface Props {
+//   title: string;
+//   className?: string;
+// }
+
+export const MaterialTable = () => {
+  // const { title, className } = props;
+  return <Table>asdasd</Table>;
+};
