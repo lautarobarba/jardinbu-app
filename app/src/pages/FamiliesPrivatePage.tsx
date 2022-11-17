d42a527d787c15b0064d84f0c0ea2ffc1682000b
Lautaro Barba <lau_mb94@hotmail.com>
2022-11-17 20:41:54 -0300

    Se agrego tabla en familias

--- app/src/pages/FamiliesPrivatePage.tsx
+++ app/src/pages/FamiliesPrivatePage.tsx
@@ -5,10 +5,25 @@ import { useQuery } from "@tanstack/react-query";
 import { Family } from "../interfaces/Family";
 import { Link } from "react-router-dom";
 import { useGetFamilies } from "../api/hooks";
+import { useEffect, useState } from "react";
+import { MDBIcon, MDBTable, MDBTableBody, MDBTableHead } from "mdb-react-ui-kit";
 
 export const FamiliesPrivatePage = () => {
+  
   // Queries
-  const { isLoading, isError, data, error } = useGetFamilies();
+  const { 
+    isLoading: getFamiliesIsLoading, 
+    isSuccess: getFamiliesIsSuccess, 
+    data: getFamiliesData, 
+    isError: getFamiliesIsError, 
+    // error: getFamiliesError 
+  } = useGetFamilies();
+
+  const [ openCreate, setOpenCreate ] = useState<boolean>(false);
+
+  const toggleCreateForm = () => {
+    setOpenCreate(!openCreate);
+  }
 
   // Mutations
   // const mutation = useMutation(postTodo, {
@@ -18,40 +33,60 @@ export const FamiliesPrivatePage = () => {
   //   },
   // });
 
-  // if (isError) {
-  //   console.log(error);
-  //   return (
-  //     <>
-  //       <PageTitle title="Familias" />
-  //       <PageSubTitle title="Listado de familias" />
-  //       <p className="h1 text-danger">Error...</p>
-  //     </>
-  //   );
-  // }
+
+  useEffect(() => {
+    if(getFamiliesIsSuccess){
+      console.log({getFamiliesData})
+    }
+  }, [getFamiliesIsSuccess, getFamiliesData]);
 
   return (
-    <>
+    <div className="bg-white p-3">
       <PageTitle title="Familias (vista privada)" />
-      <Link to="/app/admin">Volver al Dashboard</Link>
-      <PageSubTitle title="Listado de familias" />
+      <Link to="/app/admin" className="text-danger">
+        <MDBIcon fas icon="arrow-circle-left" size='2x' />
+      </Link>
+      <div className="d-flex justify-content-between">
+        <PageSubTitle title="Listado de familias" />
+        <button 
+          className="btn bg-dark text-white" 
+          style={{ maxHeight: '3rem' }}
+          onClick={toggleCreateForm}
+        >
+          { openCreate ? 'Cancelar' : 'Crear' }
+        </button>
+      </div>
 
-      {isError && (<p>ERROR</p>)}
+      {openCreate && (<p>RENDER CREATE FORM</p>)}
 
-      {isLoading ? (
-        <p className="h4">Buscando...</p>
-      ) : (
+      {getFamiliesIsError && (<p className="text-danger">Error...</p>)}
+
+      {getFamiliesIsLoading && (<p className="h4">Cargando...</p>)} 
+      
+      {getFamiliesIsSuccess && (
         <div className="families">
-          <ul>
-            {data.map((family: Family) => {
+          <MDBTable>
+            <MDBTableHead dark>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Nombre</th>
+                <th scope='col'>Descripción</th>
+              </tr>
+            </MDBTableHead>
+            <MDBTableBody>
+            {getFamiliesData.map((family: Family, index: number) => {
               return (
-                <li key={family.id}>
-                 {`${family.id}.${family.name}: ${family.description}`}
-                </li>
+                <tr key={index}>
+                  <td>{index}</td>
+                  <td>{family.name}</td>
+                  <td>{family.description}</td>
+                </tr>
               );
             })}
-          </ul>
+            </MDBTableBody>
+          </MDBTable>
         </div>
       )}
-    </>
+    </div>
   );
 };
